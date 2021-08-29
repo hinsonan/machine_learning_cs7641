594f604656393e95a820866264ade7271aa78ff1
Andrew Hinson <hinsonan@gmail.com>
2021-08-29 14:42:31 -0500

    breast cancer data done

--- supervised_learning/main.py
+++ supervised_learning/main.py
@@ -14,7 +14,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 def train_nn(Xtrain,Xtest,Ytrain,Ytest, model_name):
     model = get_model(Xtrain.shape[-1])
     model.summary()
-    model.fit(Xtrain,Ytrain, batch_size=32, epochs=500, validation_data=(Xtest,Ytest))
+    model.fit(Xtrain,Ytrain, batch_size=128, epochs=300, validation_data=(Xtest,Ytest))
     model.save(f'supervised_learning/models/{model_name}')
 
 def train_descision_tree(Xtrain,Ytrain, model_name):
@@ -56,20 +56,42 @@ def get_accuracy(model, data, truth, is_prob=False):
     score = accuracy_score(truth.flatten(), pred.flatten())
     return score
 
-df = pd.read_csv('data/csgo_round_snapshots.csv')
-one_hot_encode(df,"round_winner",['CT','T'])
-one_hot_encode(df,"map",['de_dust2', 'de_mirage', 'de_nuke', 'de_inferno', 'de_overpass', 'de_vertigo', 'de_train', 'de_cache'])
-vals = normalize_with_min_max_scaler(df)
-data = vals[:,:-1]
-labels = vals[:,-1].reshape(-1,1)
-# split the data
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(data,labels, test_size=0.33, random_state=42)
+def cs_go_training():
+    df = pd.read_csv('data/csgo_round_snapshots.csv')
+    one_hot_encode(df,"round_winner",['CT','T'])
+    one_hot_encode(df,"map",['de_dust2', 'de_mirage', 'de_nuke', 'de_inferno', 'de_overpass', 'de_vertigo', 'de_train', 'de_cache'])
+    vals = normalize_with_min_max_scaler(df)
+    data = vals[:30000,:-1]
+    labels = vals[:30000,-1].reshape(-1,1)
+    # split the data
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(data,labels, test_size=0.33, random_state=42)
 
-# train_nn(Xtrain, Xtest, Ytrain, Ytest, 'neural_net_cs_go')
-# train_descision_tree(Xtrain,Ytrain, 'decision_tree_cs_go')
-# train_SVM(Xtrain,Ytrain, 'svm_cs_go')
-# train_booster(Xtrain,Ytrain,'booster_cs_go')
-train_knn(Xtrain,Ytrain,'knn_cs_go')
-loaded_model = load_saved_model('knn_cs_go')
-score = get_accuracy(loaded_model, Xtest, Ytest)
-print(score)
+    train_nn(Xtrain, Xtest, Ytrain, Ytest, 'neural_net_cs_go')
+    # train_descision_tree(Xtrain,Ytrain, 'decision_tree_cs_go')
+    # train_SVM(Xtrain,Ytrain, 'svm_cs_go')
+    # train_booster(Xtrain,Ytrain,'booster_cs_go')
+    # train_knn(Xtrain,Ytrain,'knn_cs_go')
+    loaded_model = load_saved_model('neural_net_cs_go',True)
+    score = get_accuracy(loaded_model, Xtest, Ytest,True)
+    print(score)
+
+def breast_cancer_training():
+    df = pd.read_csv('data/breast_cancer_data.csv')
+    one_hot_encode(df,"diagnosis",['B','M'])
+    vals = normalize_with_min_max_scaler(df)
+    labels = vals[:,1].reshape(-1,1)
+    data = np.delete(vals,1,1)
+    # split the data
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(data,labels, test_size=0.33, random_state=42)
+
+    # train_nn(Xtrain, Xtest, Ytrain, Ytest, 'neural_net_breast_cancer')
+    # train_descision_tree(Xtrain,Ytrain, 'decision_tree_breast_cancer')
+    # train_SVM(Xtrain,Ytrain, 'svm_breast_cancer')
+    # train_booster(Xtrain,Ytrain,'booster_breast_cancer')
+    # train_knn(Xtrain,Ytrain,'knn_breast_cancer')
+    loaded_model = load_saved_model('knn_breast_cancer')
+    score = get_accuracy(loaded_model, Xtest, Ytest)
+    print(score)
+
+cs_go_training()
+#breast_cancer_training()
--- supervised_learning/neural_net.py
+++ supervised_learning/neural_net.py
@@ -4,10 +4,11 @@ from tensorflow.keras.optimizers import Adam, SGD, RMSprop
 
 def get_model(input_shape):
     inputs = Input(shape=(input_shape))
-    layer = Dense(16, activation='relu')(inputs)
+    layer = Dense(32, activation='relu')(inputs)
+    layer = Dense(16, activation='relu')(layer)
     layer = Dense(4, activation='relu')(layer)
     output = Dense(1, activation='sigmoid')(layer)
     model = Model(inputs, output)
     optimizer = Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer,loss='mse')
+    model.compile(optimizer=optimizer,loss='binary_crossentropy')
     return model
